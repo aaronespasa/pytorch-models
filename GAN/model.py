0e6c7fa1eb69c084def6f07c3d5f0eda5fc0b812
Aarón Espasandín <aaespas@gmail.com>
2024-02-14 11:43:54 +0100

    Training simple GAN model

---
+++ GAN/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_dim):
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(img_dim, 128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 1),
+            nn.Sigmoid() # 0 for fake, 1 for real
+        )
+    
+    def forward(self, x):
+        return self.disc(x)
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, img_dim):
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256, img_dim),
+            nn.Tanh() # normalize to -1 to 1
+        )
+    
+    def forward(self, x):
+        return self.gen(x)
